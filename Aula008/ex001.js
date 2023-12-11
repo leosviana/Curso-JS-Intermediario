ce074639aee4a67c76f7b842cd1d03564b3e9b2e
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2023-12-11 17:12:45 +0000

    alteracao de arquivos

--- Aula008/ex001.js
+++ Aula008/ex001.js
@@ -1,4 +1,4 @@
-//FUNÇÕES
+//FUNÇÕES - SOMA
 
 //É como uma pequena "fábrica" onde você faz uma entrada e ele te dá uma saída;
 //Pode ser encarado como "mini-programas" projetados para fazer uma tarefa que vai contribuir para todo o código;
