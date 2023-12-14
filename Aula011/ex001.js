917eeb04b8fb548f912631734548b3792e9fb8d7
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2023-12-14 20:53:10 +0000

    Alterando arquivos

---
+++ Aula011/ex001.js
@@ -0,0 +1,51 @@
+/* ARRAYS
+Os arrays JavaScript são usados para armazenar vários valores em uma única váriavel. 
+Diferente dos objetos, que funcionam com propriedades ou "nomes" que você dá para os itens
+dentro dele, os arrays não possuem propriedades. O item dentro dele é encontrado pela posição.
+
+Imagine um array como uma lista de itens, com controle, por ordem de posição dentro dele.
+Ex.: const lista = ["arroz", "feijão", "macarrão", "leite"];
+
+A lista[0] (lista na posição 0) vai conter o valor "arroz".
+A lista[1] (lista na posição 0) vai conter o valor "feijão".
+E assim por diante.
+
+*/
+              //0       //1       //2         //3
+const lista1 = ["arroz", "feijão", "macarrão", "leite"]; //1º FORMA
+
+const lista2 = [ //2º FORMA
+    "arroz", 
+    "feijão", 
+    "macarrão", 
+    "leite"]; 
+
+const lista3 = []; //3º FORMA
+    lista3[0] = "arroz";
+    lista3[1] = "feijão";
+    lista3[2] = "macarrão";
+    lista3[3] = "leite";
+
+const lista4 = new Array("arroz", "feijão", "macarrão", "leite"); //4º FORMA
+    lista4[3] = "café"; //Alterando valor da matrix na posição 3
+
+console.log(lista1, lista2 , lista3, lista4);
+
+//DIFERENÇA DE ARRAY PARA OBJETO
+const pessoaArray  = ["Dimitry", "Teixeira", 30]; //ARRAY - POSIÇÃO
+const pessoaObjeto = {nome: "Dimitry", sobrenome: "Teixeira", idade: 30}; //OBJETO - PROPRIEDADES
+
+pessoaArray[0]; //Carregando a posição 0 "Dimitry" do array "pessoaArray"
+pessoaObjeto.nome; //Carregando a propriedade "nome" do objeto "pessoaObjeto"
+console.log(pessoaArray[0]); //Exibindo posição 0 array
+console.log(pessoaObjeto.sobrenome); //Exibindo propriedade "sobrenome" do objeto
+
+console.log(pessoaArray.length); //Chamando o método "length" da propriedade para de trazer o tamanho do array
+console.log(pessoaArray[pessoaArray.length -1]); //Trazendo sempre a última posição do array [tamanho do array -1]
+
+//INSERINDO REGISTRO NO ARRAY
+const pessoaArray2 = ["Ricardo", "Silveira", "44"];
+pessoaArray2.push("Brasileiro"); //Chamando o método "push" para inserir um registro em uma nova posição no array.
+pessoaArray2[pessoaArray2.length] = "Casado"; //Outra forma de inserir um registro em uma nova posição no array.
+console.log(pessoaArray2);
+console.log(Array.isArray(pessoaArray2)); //Exibindo se a variável é um array ou não (True ou False).
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Curso de JS Intermediário do canal do Youtube "Programacao WEB" Aqui irei colocar todo os exercícios e projetos relacionados ao conteúdo do curso.
 
 CURSO: https://www.youtube.com/watch?v=rwJ2bwsjGhQ&list=PL2Fdisxwzt_d590u3uad46W-kHA0PTjjw&index=10<br>
-Aulas 008 - Concluída 11/12/23<br>
-Aulas 009 - Concluída 11/12/23<br>
-Aulas 010 - Concluída 11/12/23<br>
+Aula 008 - Concluída 11/12/23<br>
+Aula 009 - Concluída 11/12/23<br>
+Aula 010 - Concluída 14/12/23<br>
+Aula 011 - Iniciando..<br>
