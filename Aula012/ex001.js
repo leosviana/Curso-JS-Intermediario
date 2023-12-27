9fed48b8391c2c402156b275cdfc8cf7a8451a96
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2023-12-26 22:47:29 -0300

    Update ex001.js

--- Aula012/ex001.js
+++ Aula012/ex001.js
@@ -44,6 +44,40 @@ const jogadores = [
 const craques = jogadores.slice(2,6); //Fatiar array a partir da posição 2 até posição 6
 document.getElementById("campo3").innerHTML = craques;
 
-//MÉTODO: SORT - Ordenar array
+//MÉTODO: SORT (PARA TEXTOS) - Ordenar array
 const ordemJogadores = jogadores.sort();
 document.getElementById("campo4").innerHTML = ordemJogadores;
+
+//MÉTODO: SORT (PARA TEXTOS) - Ordenar array decrescente
+const ordemJogadores2 = jogadores.reverse();
+document.getElementById("campo5").innerHTML = ordemJogadores2;
+
+//MÉTODO: SORT (PARA TEXTOS) - Ordenar array numérico
+const numeros = [40, 100, 1, 5, 25, 10];
+document.getElementById("campo6").innerHTML = numeros.sort(function(a,b){return a-b}); //Compara se o numero é maior ou menor que o anterior e deixa na ordem crescente
+
+//MÉTODO: SORT (PARA TEXTOS) - Ordenar array numérico decrescente
+const numeros2 = [40, 100, 1, 5, 25, 10];
+document.getElementById("campo7").innerHTML = numeros2.sort(function(a,b){return b-a}); //Compara se o numero é maior ou menor que o anterior e deixa na ordem decrescente
+
+//FUNÇÃO: Math.max.apply - Pegar o maior numero
+const numeros3 = [40, 100, 1, 5, 25, 10];
+function MaiorNumero(array){
+    return Math.max.apply(null, array);
+}
+document.getElementById("campo8").innerHTML = MaiorNumero(numeros3);
+
+//FUNÇÃO: Math.min.apply - Pegar o menor numero
+const numeros4 = [40, 100, 1, 5, 25, 10];
+function MenorNumero(array){
+    return Math.min.apply(null, array);
+}
+document.getElementById("campo9").innerHTML = MenorNumero(numeros4);
+
+//FUNÇÃO: filter - Filtrar numeros
+const numeros5 = [40, 100, 1, 5 , 25, 10];
+const maior20 = numeros5.filter(filtragem);
+function filtragem(value, index, array){
+    return value > 20;
+}
+document.getElementById("campo10").innerHTML = maior20;
