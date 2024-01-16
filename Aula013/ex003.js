d7e891f83088b3e4a20b6acd2efbec810887a1ec
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-01-16 19:50:00 +0000

    alterando arquivos

--- Aula013/ex003.js
+++ Aula013/ex003.js
@@ -1,9 +1,13 @@
 function verificar(){
-    let nome = document.getElementById("campo3").value; //capturando o valor do campo
+    let nome = document.getElementById("nome").value; //capturando o valor do campo
 
     if (nome=="" || nome==null){
         let p = document.getElementById("campo3");
         p.innerHTML = "O campo não pode ser vazio";
         p.style.color = "red";
+    }else{
+        let p = document.getElementById("campo3");
+        p.innerHTML = "Parabéns, preenchido certinho!";
+        p.style.color = "green";
     }
 }
