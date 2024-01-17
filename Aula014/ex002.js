7c988f2045725f13371dea514745bf8502de7b2f
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2024-01-17 06:55:25 -0300

    Update ex002.js

--- Aula014/ex002.js
+++ Aula014/ex002.js
@@ -1,15 +1,16 @@
 function verificaValor(){
-    let valor = document.getElementById("valor").value;
-    
+        
+    //valor = 0;
+    valor = "0";
     //swith é literal. Utiliza-se de comparação o ===
     switch(valor){ //Verifica o tipo da variável
         case "0": //zero em formato de string
             //Exibe a descrição no campo2
             document.getElementById("campo2").innerHTML = "A variável é 0 em formato string";
             break;
-        default:
-            //Exibe o valor padrão
-            document.getElementById("campo2").innerHTML = "Valor diferente de 0";
+        case 0:
+            document.getElementById("campo2").innerHTML  = "A variável é 0 em formato numérico";
+            break;
     }
 
 
