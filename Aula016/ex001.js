94001ae0be94bac39af0239bb3d34ad5022e773d
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2024-01-20 12:54:47 -0300

    Update ex001.js

--- Aula016/ex001.js
+++ Aula016/ex001.js
@@ -2,8 +2,26 @@
 //ATIVA A FUNÇÃO APENAS 1 VEZ QUANDO DER O TEMPO ESPECIFICADO
 function start(){
     document.getElementById("contagem").innerHTML = "Começou a contar";
-    setTimeout(function(){
+    contagem = setTimeout(function(){
         document.getElementById("contagem").innerHTML = "Executou o setTimout";
         document.body.style.backgroundColor = "yellow";
     }, 5000);
 }
+
+//PAUSA A FUNÇÃO APENAS 1 VEZ QUANDO DER O TEMPO ESPECIFICADO
+function stop(){
+    clearTimeout(contagem);
+    clearInterval(contagem);
+    document.getElementById("contagem").innerHTML = "Parou a contagem";
+    document.body.style.backgroundColor = "Blue";
+}
+
+//EXECUTA REPETIDAMENTE DE A CORDO COM INTERVALO DE TEMPO INFORMADO
+function intervalo(){ 
+    setInterval(function() {
+        var cronometro = document.getElementById("contagem").innerHTML;
+        var soma = parseInt(cronometro) + 1;
+        document.getElementById("contagem").innerHTML = soma;
+}, 1000);
+}
+
