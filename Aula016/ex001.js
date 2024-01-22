e9055ae4ac885c488fbd93d9f18f1a3704b6d9a3
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-01-22 20:21:21 +0000

    atualizando arquivos

--- Aula016/ex001.js
+++ Aula016/ex001.js
@@ -1,53 +1,53 @@
 
 //setTimeout - ATIVA A FUNÇÃO APENAS 1 VEZ QUANDO DER O TEMPO ESPECIFICADO
-function start(){
-    document.getElementById("contagem").innerHTML = "Começou a contar";
-    contagem = setTimeout(function(){
-        document.getElementById("contagem").innerHTML = "Executou o setTimout";
-        document.body.style.backgroundColor = "yellow";
-    }, 5000);
+function start(){ //FUNÇÃO DE CONTAGEM SIMPLES
+    document.getElementById("contagem").innerHTML = "Começou a contar"; //Exibe informação
+    contagem = setTimeout(function(){  //Armazena o valor resultante da função na variável "contagem"
+        document.getElementById("contagem").innerHTML = "Executou o setTimout"; //Exibe informação
+        document.body.style.backgroundColor = "yellow"; //Pinta o fundo de amarelo
+    }, 5000); //Contar até 5 segundos
 }
 
 //clearTimeout - PAUSA A FUNÇÃO APENAS 1 VEZ QUANDO DER O TEMPO ESPECIFICADO
-function stop(){
-    clearTimeout(contagem);   
-    document.getElementById("contagem").innerHTML = "0";
-    document.body.style.backgroundColor = "Blue";
+function stop(){ //Função de pausar a contagem
+    clearTimeout(contagem); //Executa função do JS. Limpa o valor da variável "contagem"
+    document.getElementById("contagem").innerHTML = "0"; //Exibe o valor 0 para iniciar a contagem novamente   
+    document.body.style.backgroundColor = "Blue"; //Pinta o fundo de azul
 }
 
 //setInterval - EXECUTA REPETIDAMENTE DE A CORDO COM INTERVALO DE TEMPO INFORMADO
-function start2(){ 
-    contagem = setInterval(function() {
-        var cronometro = document.getElementById("contagem").innerHTML;
-        var soma = parseInt(cronometro) + 1;
-        document.getElementById("contagem").innerHTML = soma;
-}, 1000);
+function start2(){ //FUNÇÃO DE CONTAGEM PROGRESSIVA
+    contagem = setInterval(function() { //Armazena o valor resultante da função na variável "contagem"
+        var cronometro = document.getElementById("contagem").innerHTML; //Captura o valor do campo texto do HTML
+        var soma = parseInt(cronometro) + 1; //Converte o campo texto em numero inteiro e soma + 1
+        document.getElementById("contagem").innerHTML = soma; //Exibe o valor atualizado da contagem
+}, 1000); //Contar de 1 em 1 segundo
 }
 
 //clearInterval - PAUSA A FUNÇÃO DE INTERVALO
-function stop2(){ 
-    clearInterval(contagem);
-    document.getElementById("contagem").innerHTML = "0";
+function stop2(){ //Função de pausar a contagem
+    clearInterval(contagem); //Executa função do JS. Limpa o valor da variável "contagem"
+    document.getElementById("contagem").innerHTML = "0"; //Exibe o valor 0 para iniciar a contagem novamente   
 }
 
 //setInterval - EXECUTA REPETIDAMENTE DE A CORDO COM INTERVALO DE TEMPO INFORMADO
-function start3(){ 
-    document.getElementById("contagem").innerHTML = "100";  
-    contagem = setInterval(function() {        
-        var cronometro = document.getElementById("contagem").innerHTML;
-        var subtrair = parseInt(cronometro) - 1;
+function start3(){ //FUNÇÃO DE CONTAGEM REGRESSIVA
+    document.getElementById("contagem").innerHTML = "100"; //Exibe o valor 100 no campo de contagem 
+    contagem = setInterval(function() { //Armazena o valor resultante da função na variável "contagem"        
+        var cronometro = document.getElementById("contagem").innerHTML; //Captura o valor do campo texto do HTML
+        var subtrair = parseInt(cronometro) - 1; //Converte o campo texto em numero inteiro e subtrai - 1
 
-        if(subtrair === 0){
-            document.getElementById("contagem").innerHTML = "Tempo esgotado";
-            stop3();            
-        }else{
-            document.getElementById("contagem").innerHTML = subtrair;
+        if(subtrair === 0){ //Se o valor for igual à 0
+            document.getElementById("contagem").innerHTML = "Tempo esgotado"; //Exibe informação
+            stop3(); //Executa a função de pausar a contagem
+        }else{ //Se não
+            document.getElementById("contagem").innerHTML = subtrair; //Exibe o valor atualizado da contagem
         }        
-}, 1000);
+}, 1000); //Contar de 1 em 1 segundo
 }
 
 //clearInterval - PAUSA A FUNÇÃO DE INTERVALO
-function stop3(){
-    clearInterval(contagem);
-    document.getElementById("contagem").innerHTML = "0";    
+function stop3(){ //Função de pausar a contagem
+    clearInterval(contagem); //Executa função do JS. Limpa o valor da variável "contagem"
+    document.getElementById("contagem").innerHTML = "0";  //Exibe o valor 0 para iniciar a contagem novamente   
 }
