f2af6d71418c4149f14222d0fdf09312d071433a
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2024-01-22 07:23:38 -0300

    Update ex001.js

--- Aula016/ex001.js
+++ Aula016/ex001.js
@@ -11,13 +11,13 @@ function start(){
 //clearTimeout - PAUSA A FUNÇÃO APENAS 1 VEZ QUANDO DER O TEMPO ESPECIFICADO
 function stop(){
     clearTimeout(contagem);   
-    document.getElementById("contagem").innerHTML = "Parou a contagem";
+    document.getElementById("contagem").innerHTML = "0";
     document.body.style.backgroundColor = "Blue";
 }
 
 //setInterval - EXECUTA REPETIDAMENTE DE A CORDO COM INTERVALO DE TEMPO INFORMADO
 function start2(){ 
-    setInterval(function() {
+    contagem = setInterval(function() {
         var cronometro = document.getElementById("contagem").innerHTML;
         var soma = parseInt(cronometro) + 1;
         document.getElementById("contagem").innerHTML = soma;
@@ -27,6 +27,27 @@ function start2(){
 //clearInterval - PAUSA A FUNÇÃO DE INTERVALO
 function stop2(){ 
     clearInterval(contagem);
-    document.getElementById("contagem").innerHTML = "Parou a contagem";
-    document.body.style.backgroundColor = "PURPLE";
+    document.getElementById("contagem").innerHTML = "0";
+}
+
+//setInterval - EXECUTA REPETIDAMENTE DE A CORDO COM INTERVALO DE TEMPO INFORMADO
+function start3(){ 
+    document.getElementById("contagem").innerHTML = "100";  
+    contagem = setInterval(function() {        
+        var cronometro = document.getElementById("contagem").innerHTML;
+        var subtrair = parseInt(cronometro) - 1;
+
+        if(subtrair === 0){
+            document.getElementById("contagem").innerHTML = "Tempo esgotado";
+            stop3();            
+        }else{
+            document.getElementById("contagem").innerHTML = subtrair;
+        }        
+}, 1000);
+}
+
+//clearInterval - PAUSA A FUNÇÃO DE INTERVALO
+function stop3(){
+    clearInterval(contagem);
+    document.getElementById("contagem").innerHTML = "0";    
 }
