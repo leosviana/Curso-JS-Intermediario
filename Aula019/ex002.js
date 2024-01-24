56dc8f86ab3eb2b5fa7cd74e5ff5b74fc155ecd0
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-01-24 20:58:04 +0000

    alterando arquivos

--- Aula019/ex002.js
+++ Aula019/ex002.js
@@ -1,10 +1,14 @@
 
+// API - VIA CEP
 //Acessando um site de forma remota
+// https://viacep.com.br/
 const ajax = new XMLHttpRequest(); //Capturando objeto de requisição 
 ajax.open('GET', 'https://viacep.com.br/ws/01001000/json/'); //Insirindo caminho do JSON
     //GET - Pegar informações
 ajax.send(); //Enviar informações
 
-ajax.onload = function{ //Carregou as informações do GET
-    document.getElementById("respostaGET").innerHTML = this.responseText;
+ajax.onload = function(){ //Carregou as informações do GET
+    document.getElementById("respostaGET").innerHTML = "Capturado o JSON em formato texto: </br>" + this.responseText;
+    let obj = JSON.parse(this.responseText); //Transformando texto em objeto para manipulação
+    document.getElementById("respostaGETManipulado").innerHTML = "Capturado apenas o DDD: " + obj.ddd;
 }
